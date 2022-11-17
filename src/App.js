563b92c15da301eef27a51b9317bec2b514e3ed3
mukesh kumar <mukesh.chhimpa11@gmail.com>
2022-11-17 10:40:49 +0530

    can toggle tasks

--- src/App.js
+++ src/App.js
@@ -1,36 +1,45 @@
-import { useState,useRef } from "react";
+import { useState, useRef } from "react";
 import TodoList from "./TodoList";
-import { v4 as uuidv4 } from 'uuid';
-const LOCAL_STORAGE_KEY='todoApp.todos'
+import { v4 as uuidv4 } from "uuid";
+const LOCAL_STORAGE_KEY = "todoApp.todos";
 
 function App() {
-  const initStage = localStorage.getItem(LOCAL_STORAGE_KEY) ? JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY)) : []
-  const [todos,setTodo]= useState(initStage)
-  const todoNameRef=useRef()
- 
-  
+  const initStage = localStorage.getItem(LOCAL_STORAGE_KEY)
+    ? JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY))
+    : [];
+  const [todos, setTodo] = useState(initStage);
+  const todoNameRef = useRef();
 
-  function handleAddTodo(e){
-    const name = todoNameRef.current.value
-    if(name ==='')
-    return
-    setTodo(prevTodos =>{
-      let newTodos = [...prevTodos,{id:uuidv4(),name:name,complete:false}]
-      localStorage.setItem(LOCAL_STORAGE_KEY,JSON.stringify(newTodos))
-      return newTodos
-    })
-    todoNameRef.current.value =null
+  function handleAddTodo(e) {
+    const name = todoNameRef.current.value;
+    if (name === "") return;
+    setTodo((prevTodos) => {
+      let newTodos = [
+        ...prevTodos,
+        { id: uuidv4(), name: name, complete: false },
+      ];
+      localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(newTodos));
+      return newTodos;
+    });
+    todoNameRef.current.value = null;
+  }
+  function toggleTodo(id){
+    const newTodos=[...todos]
+    const todo = newTodos.find(todo => todo.id === id)
+    todo.complete = !todo.complete
+    setTodo(newTodos)
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(newTodos));
   }
   console.log("rendered");
   return (
-      <>
-         <TodoList todos={todos}/>
-         <input ref={todoNameRef} type="text" />
-         <button onClick={handleAddTodo}>Add Task</button>
-         <button>Clear</button>
-         <div>Number of tasks 0</div>
-      </>
-    )
+    <>
+      <TodoList todos={todos} toggleTodo = {toggleTodo}/>
+      <input ref={todoNameRef} type="text" />
+      <button onClick={handleAddTodo}>Add Task</button>
+      <button>Clear</button>
+      <div>Number of tasks 0</div>
+    </>
+  );
 }
 
 export default App;
